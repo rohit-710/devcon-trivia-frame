22740c05d4af74ba9f60c6e0cc66737a7ee20074
Rohit Ramesh <rrohitramesh710@gmail.com>
2024-06-23 17:44:55 +0200

    fixing code mismatch

--- src/app/api/end/route.ts
+++ src/app/api/end/route.ts
@@ -1,127 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
-const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL;
-
-async function getResponse(req: NextRequest): Promise<NextResponse> {
-  const searchParams = req.nextUrl.searchParams;
-  const idString: any = searchParams.get("id");
-  const id = parseInt(idString);
-  const nextId = id + 1;
-  const data = await req.json();
-  const buttonId = data.untrustedData?.buttonIndex;
-
-  const answerOptions = [
-    ["2014", "2015", "2016"],
-    ["Berlin", "London", "Shanghai"],
-    ["Ether Bunny", "Deva the Unicorn", "CryptoKitty"],
-  ];
-
-  const correctAnswers = [0, 1, 1];
-
-  if (id === 0) {
-    // Display the first frame with question options
-    return new NextResponse(`
-      <!DOCTYPE html>
-      <html>
-      <head>
-      <title>This is frame ${id}</title>
-      <meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:image" content="${NEXT_PUBLIC_URL}/${id}.png"/>
-
-      <meta property="fc:frame:button:1" content=${answerOptions[id][0]} />
-      <meta property="fc:frame:button:2" content=${answerOptions[id][1]} />
-      <meta property="fc:frame:button:3" content=${answerOptions[id][2]} />
-
-      <meta property="fc:frame:image:aspect_ratio" content="1.91:1" />
-      <meta property="fc:frame:post_url" content="${NEXT_PUBLIC_URL}/api/frame?id=${nextId}" />
-      </head>
-      </html>`);
-  }
-
-  // Check if the answer is correct
-  if (id > 0 && buttonId - 1 === correctAnswers[id - 1] && id < 4) {
-    return new NextResponse(`
-      <!DOCTYPE html>
-      <html>
-      <head>
-      <title>Correct! Move to the next question.</title>
-      <meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:image" content="${NEXT_PUBLIC_URL}/correct.png" />
-      <meta property="fc:frame:button:1" content="Next question"} />
-      <meta property="fc:frame:post_url" content="${NEXT_PUBLIC_URL}/api/frame?id=${nextId}" />
-      <meta property="fc:frame:image:aspect_ratio" content="1.91:1" />
-      </head>
-      </html>`);
-  }
-
-  // Check if the answer is incorrect
-  if (id > 0 && buttonId - 1 !== correctAnswers[id - 1] && id < 4) {
-    return new NextResponse(`
-      <!DOCTYPE html>
-      <html>
-      <head>
-      <title>Wrong! Try the next question.</title>
-      <meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:image" content="${NEXT_PUBLIC_URL}/wrong.png" />
-      <meta property="fc:frame:button:1" content="Next question"} />
-      <meta property="fc:frame:post_url" content="${NEXT_PUBLIC_URL}/api/frame?id=${nextId}" />
-      <meta property="fc:frame:image:aspect_ratio" content="1.91:1" />
-      </head>
-      </html>`);
-  }
-
-  // Check if this is the final frame
-  if (id === 4) {
-    return new NextResponse(`
-      <!DOCTYPE html>
-      <html>
-      <head>
-      <title>The End</title>
-      <meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:image" content="${NEXT_PUBLIC_URL}/end.png" />
-      <meta property="fc:frame:button:1" content="Play again"} />
-      <meta property="fc:frame:post_url" content="${NEXT_PUBLIC_URL}/api/end" />
-      <meta property="fc:frame:image:aspect_ratio" content="1.91:1" />
-      </head>
-      </html>`);
-  }
-
-  // Default case for frames with questions
-  if (id > 0 && id < 4) {
-    return new NextResponse(`
-      <!DOCTYPE html>
-      <html>
-      <head>
-      <title>This is frame ${id}</title>
-      <meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:image" content="${NEXT_PUBLIC_URL}/${id}.png"/>
-
-      <meta property="fc:frame:button:1" content=${answerOptions[id][0]} />
-      <meta property="fc:frame:button:2" content=${answerOptions[id][1]} />
-      <meta property="fc:frame:button:3" content=${answerOptions[id][2]} />
-
-      <meta property="fc:frame:image:aspect_ratio" content="1.91:1" />
-      <meta property="fc:frame:post_url" content="${NEXT_PUBLIC_URL}/api/frame?id=${nextId}" />
-      </head>
-      </html>`);
-  }
-
-  // Return a default response in case of unexpected input
-  return new NextResponse(`
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <title>Error</title>
-    <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${NEXT_PUBLIC_URL}/error.png" />
-    <meta property="fc:frame:button:1" content="Start again"} />
-    <meta property="fc:frame:post_url" content="${NEXT_PUBLIC_URL}/api/frame?id=0" />
-    <meta property="fc:frame:image:aspect_ratio" content="1.91:1" />
-    </head>
-    </html>`);
-}
 
 export async function POST(req: NextRequest): Promise<Response> {
-  return getResponse(req);
+  const headers = new Headers();
+  headers.set("Location", `${process.env.NEXT_PUBLIC_URL}/`);
+  const response = NextResponse.redirect(`${process.env.NEXT_PUBLIC_URL}`, {
+    headers: headers,
+    status: 302,
+  });
+  return response;
 }
 
 export const dynamic = "force-dynamic";
