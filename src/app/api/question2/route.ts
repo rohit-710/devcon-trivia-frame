64821daca3cb6258801092e7c96f314b30b45c19
Rohit Ramesh <rrohitramesh710@gmail.com>
2024-06-23 18:44:17 +0200

    alter logic

--- src/app/api/question2/route.ts
+++ src/app/api/question2/route.ts
@@ -4,7 +4,7 @@ const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL;
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   const searchParams = req.nextUrl.searchParams;
   const idString: any = searchParams.get("id");
-  const id = parseInt(idString) || 1;
+  const id = parseInt(idString);
   const nextId = id + 1;
   const data = await req.json();
   const buttonId = data.untrustedData.buttonIndex;
@@ -17,24 +17,8 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
 
   const correctAnswers = [0, 1, 1];
 
-  // Check if this is the final frame
-  if (id === 4) {
-    return new NextResponse(`
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <title>The End</title>
-    <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${NEXT_PUBLIC_URL}/end.png" />
-    <meta property="fc:frame:button:1" content="Play again"} />
-    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_URL}/api/end" />
-    <meta property="fc:frame:image:aspect_ratio" content="1.91:1" />
-    </head>
-    </html>`);
-  }
-
   // Check if the answer is incorrect
-  if (buttonId - 1 !== correctAnswers[id - 1]) {
+  if (id > 1 && buttonId - 1 !== correctAnswers[id - 2]) {
     return new NextResponse(`
         <!DOCTYPE html>
         <html>
@@ -49,8 +33,8 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         </html>`);
   }
 
-  // Check if the answer is correct
-  if (buttonId - 1 === correctAnswers[id - 1]) {
+  // Check if the answer is correct and not the final frame
+  if (id > 1 && buttonId - 1 === correctAnswers[id - 2] && id < 4) {
     return new NextResponse(`
         <!DOCTYPE html>
         <html>
@@ -65,27 +49,46 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         </html>`);
   }
 
-  // Display the question frame
-  return new NextResponse(`
-      <!DOCTYPE html>
-      <html>
-      <head>
-      <title>This is frame ${id}</title>
-      <meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:image" content="${NEXT_PUBLIC_URL}/${id}.png"/>
-      <meta property="fc:frame:button:1" content="${
-        answerOptions[id - 1][0]
-      }" />
-      <meta property="fc:frame:button:2" content="${
-        answerOptions[id - 1][1]
-      }" />
-      <meta property="fc:frame:button:3" content="${
-        answerOptions[id - 1][2]
-      }" />
-      <meta property="fc:frame:image:aspect_ratio" content="1.91:1" />
-      <meta property="fc:frame:post_url" content="${NEXT_PUBLIC_URL}/api/frame?id=${nextId}" />
-      </head>
-      </html>`);
+  // Check if this is the final frame
+  if (id === 4) {
+    return new NextResponse(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <title>The End</title>
+    <meta property="fc:frame" content="vNext" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_URL}/end.png" />
+    <meta property="fc:frame:button:1" content="Play again"} />
+    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_URL}/api/end" />
+    <meta property="fc:frame:image:aspect_ratio" content="1.91:1" />
+    </head>
+    </html>`);
+  } else {
+    return new NextResponse(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <title>This is frame ${id}</title>
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="${
+          process.env.NEXT_PUBLIC_URL
+        }/${id}.png"/>
+        <meta property="fc:frame:button:1" content=${
+          answerOptions[id - 1][0]
+        } />
+        <meta property="fc:frame:button:2" content=${
+          answerOptions[id - 1][1]
+        } />
+        <meta property="fc:frame:button:3" content=${
+          answerOptions[id - 1][2]
+        } />
+        <meta property="fc:frame:image:aspect_ratio" content="1.91:1" />
+        <meta property="fc:frame:post_url" content="${
+          process.env.NEXT_PUBLIC_URL
+        }/api/frame?id=${nextId}" />
+        </head>
+        </html>`);
+  }
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
