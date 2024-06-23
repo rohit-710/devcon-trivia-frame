26fd2760c4ceaed19e774255f9e8d78e02ec3a83
Rohit Ramesh <rrohitramesh710@gmail.com>
2024-06-23 20:06:49 +0200

    altering url

--- src/app/api/end/route.ts
+++ src/app/api/end/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest): Promise<Response> {
   const headers = new Headers();
-  headers.set("Location", `${process.env.NEXT_PUBLIC_URL}/`);
+  headers.set("Location", `${process.env.NEXT_PUBLIC_URL}`);
   const response = NextResponse.redirect(`${process.env.NEXT_PUBLIC_URL}`, {
     headers: headers,
     status: 302,
