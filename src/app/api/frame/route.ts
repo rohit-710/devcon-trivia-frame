c292d6a544ace36ed7bb65d88e4f6645bfa25a5e
Rohit Ramesh <rrohitramesh710@gmail.com>
2024-06-25 14:55:49 +0200

    rollback

--- src/app/api/frame/route.ts
+++ src/app/api/frame/route.ts
@@ -6,14 +6,11 @@ import {
 } from "@coinbase/onchainkit";
 const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL;
 
-let retryingQuestionId: number | null = null; // Stores the ID of the question being retried (can be null)
-let retryCount: number = 0; // Tracks the number of retries for the current question
-
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   const searchParams = req.nextUrl.searchParams;
   const idString: any = searchParams.get("id");
   const id = parseInt(idString);
-
+  const nextId = id + 1;
   const data = await req.json();
   const buttonId = data.untrustedData.buttonIndex;
 
@@ -30,31 +27,26 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
 
   const correctAnswers = [0, 1, 2, 1, 2, 3, 1, 1];
 
-  // Check if retrying a question
-  const isRetrying = retryingQuestionId !== null && retryingQuestionId === id;
-
-  if (isRetrying) {
-    retryCount++; // Increment retry count
-  } else {
-    retryingQuestionId = id; // Set retry state for first attempt on a question
-    retryCount = 0; // Reset retry count for new question
+  if (id > 1 && buttonId - 1 !== correctAnswers[id - 2]) {
+    return new NextResponse(
+      getFrameHtmlResponse({
+        image: {
+          src: `${process.env.NEXT_PUBLIC_URL}/wrong${id - 1}.png`,
+          aspectRatio: "1.91:1",
+        },
+        ogTitle: "Wrong! Try again.",
+        postUrl: `${process.env.NEXT_PUBLIC_URL}/api/frame?id=1`,
+        buttons: [
+          {
+            label: "Play again",
+            action: "post",
+          },
+        ],
+      })
+    );
   }
 
-  // Determine if user answered correctly (consider retries)
-  const answeredCorrectly =
-    buttonId - 1 === correctAnswers[id - 2] || retryCount >= 2;
-
-  if (answeredCorrectly) {
-    retryingQuestionId = null; // Clear retry state after correct answer
-    retryCount = 0; // Reset retry count after correct answer
-  }
-
-  // Ensure image for first question is displayed for initial render and retry
-  const selectedImageId = isRetrying && id === 1 ? 1 : id; // Use 1 for first question retry
-
-  // Show appropriate content based on question, retry state, and answer
-  if (id === 9 || (answeredCorrectly && id > 1)) {
-    // Check for answered correctly and avoid early jump
+  if (id === 9) {
     return new NextResponse(
       getFrameHtmlResponse({
         image: {
@@ -64,7 +56,6 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         ogTitle: "You won",
         postUrl: `${process.env.NEXT_PUBLIC_URL}/api/frame?id=1`,
         buttons: [
-          // Replace with your actual buttons
           {
             label: "Follow Rohit",
             action: "link",
@@ -75,6 +66,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
             action: "link",
             target: "https://warpcast.com/rosee",
           },
+
           {
             label: "Devcon Channel",
             action: "link",
@@ -87,27 +79,26 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     return new NextResponse(
       getFrameHtmlResponse({
         image: {
-          src: `${NEXT_PUBLIC_URL}/${selectedImageId}.png`,
+          src: `${process.env.NEXT_PUBLIC_URL}/${id}.png`,
           aspectRatio: "1.91:1",
         },
         ogTitle: `This is frame ${id}`,
-        postUrl: `${NEXT_PUBLIC_URL}/api/frame?id=${id === 9 ? 1 : id + 1}`,
+        postUrl: `${process.env.NEXT_PUBLIC_URL}/api/frame?id=${nextId}`,
         buttons: [
-          // Replace with your actual buttons (answer options)
           {
-            label: `${answerOptions[selectedImageId - 1][0]}`, // Use selectedImageId for answer options
+            label: `${answerOptions[id - 1][0]}`,
             action: "post",
           },
           {
-            label: `${answerOptions[selectedImageId - 1][1]}`,
+            label: `${answerOptions[id - 1][1]}`,
             action: "post",
           },
           {
-            label: `${answerOptions[selectedImageId - 1][2]}`,
+            label: `${answerOptions[id - 1][2]}`,
             action: "post",
           },
           {
-            label: `${answerOptions[selectedImageId - 1][3]}`,
+            label: `${answerOptions[id - 1][3]}`,
             action: "post",
           },
         ],
